59c39b8fb03b73a07a11a6f112bcadbb97ba0912
stevensanche <115494636+stevensanche@users.noreply.github.com>
2024-01-23 11:54:02 -0800

    Add files via upload

---
+++ cis122p12.py
@@ -0,0 +1,27 @@
+'''
+CIS 122 Spring 2022 Project 1-2
+Author(s): Steven Sanchez-Jimenez, C. Science
+Description: Intro to computational problem-solving: use Python
+numeric data types and operations to solve small problems.
+'''
+total_people = 100
+'''This is just to remember how many people there are in total'''
+total_children = 40
+'''Knowing there are 40 children, I can assume the rest are adults'''
+total_adult = 60
+one_child = 3
+one_adult = 2
+one_watermelon = 10
+children_watermelon = total_children * one_child
+adult_watermelon = total_adult * one_adult
+'''After running the code above, I was able to see that I had in total 24 watermelon,
+and with that number, I am able to assume that the extra watermelon left over is 2
+watermelon, since the final answer is 26 and without the extra, I got 24'''
+extra_watermelon = 2
+total_watermelon = (children_watermelon + adult_watermelon)/10 +(extra_watermelon)
+print(total_watermelon)
+
+
+x = 25/5
+print (x)
+
